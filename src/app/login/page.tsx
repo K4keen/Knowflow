80bab94b2dc74d1e7375927494c563dd1e0e9200
Arthur Kan <arthurkan@163.com>
2025-09-20 15:08:12 -0700

    Eslint minor change

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -17,7 +17,7 @@ export default function LoginPage() {
                         <LoginForm />
                         <div className="mt-4 text-center">
                             <p className="text-sm text-gray-600 dark:text-gray-400">
-                                Don't have an account?{" "}
+                                Don&apos;t have an account?{" "}
                                 <Link href="/register" className="text-blue-600 hover:text-blue-500 font-medium">
                                     Sign up
                                 </Link>
